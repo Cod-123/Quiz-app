54418f0ad1c294fbea963eab4468f50976a25b12
Vishal <104760817+Cod-123@users.noreply.github.com>
2022-08-04 21:13:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is 10 + 10?',
+    answers: [
+      { text: '20', correct: true },
+      { text: '32', correct: false }
+    ]
+  },
+  {
+    question: 'Which animal is regarded as the national animal of india?',
+    answers: [
+      { text: 'Elephant', correct: false },
+      { text: 'Tiger', correct: true },
+      { text: 'lion', correct: false },
+      { text: 'deer', correct: false }
+    ]
+  },
+  {
+    question: 'Which is the correct notation for indian currency?',
+    answers: [
+      { text: 'Dollar', correct: false },
+      { text: 'Rupee', correct: true },
+      { text: 'Yen', correct: false },
+      { text: 'Pound', correct: false }
+    ]
+  },
+  {
+    question: 'What is 12 * 10?',
+    answers: [
+      { text: '60', correct: false },
+      { text: '120', correct: true }
+    ]
+  }
+]
